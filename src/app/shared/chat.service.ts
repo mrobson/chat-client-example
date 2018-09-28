010fc7d2d775e10f3200129c648a429b4e7be38c
Jooho Lee <jlee@redhat.com>
2018-09-27 20:31:53 -0400

    add CORS

--- src/app/shared/chat.service.ts
+++ src/app/shared/chat.service.ts
@@ -2,10 +2,11 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import * as io from 'socket.io-client';
 import {Message} from './message.model';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import 'rxjs/add/operator/map';
 import {Subject} from 'rxjs';
+
 
 @Injectable()
 export class ChatService {
@@ -24,7 +25,7 @@ export class ChatService {
 
   public getMessages() {
     const observable = new Observable(observer => {
-      this.socket = io( this.nodeJsUrl );
+      this.socket = io(this.nodeJsUrl);
       this.socket.on('message', (message: Message) => {
         observer.next(message);
       });
@@ -42,10 +43,15 @@ export class ChatService {
 
 
   public getAllMessages() {
+    const headers: HttpHeaders = new HttpHeaders();
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    headers.append('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
 
-    this.httpClient.get( this.nodeJsUrl + ':3000/get_messages', {
+    this.httpClient.get(this.nodeJsUrl + ':3000/get_messages', {
       responseType: 'json',
       observe: 'body',
+      headers: headers
     }).subscribe(
       (data: Message[]) => {
         this.messageList = data;
